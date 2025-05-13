73d27ade22a77933bd36199f48c74dfac440de74
RabaniGadani <rabanigadani@gmail.com>
2025-05-13 10:22:51 +0500

    Update subFooter.tsx

--- src/app/components/subFooter.tsx
+++ src/app/components/subFooter.tsx
@@ -2,10 +2,10 @@ export default function Footer1() {
     return (
       <footer className="w-full text-center py-4 border-2  text-sm text-gray-500 bg-white">
         <p>
-          © {new Date().getFullYear()} Quick Tech Institute Of Information Technology. All rights reserved.
+         Quick Tech Institute Of Information Technology. All rights reserved © {new Date().getFullYear()} 
         </p>
         <p className="mt-1">
-          Powered by <span className="font-medium text-white-700">Agentic AI Engineer,Muhammad Essa Gadani</span>
+          Powered by <span className="font-medium text-white-700">Agentic AI Eng,Muhammad Essa Gadani</span>
         </p>
       </footer>
     );
